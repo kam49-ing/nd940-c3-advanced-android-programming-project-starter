d70ef2e3b959ebf185beb5651ea9f590477dc5b2
Ngomdé cadet KAMDAOU <nkamdaou@gmail.com>
2021-11-18 15:57:29 +0200

    nanodegree project 3

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,10 +1,14 @@
 package com.udacity
 
+import android.animation.AnimatorInflater
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,20 +17,30 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var valueAnimator = ValueAnimator()
+
+    private var progress = 0.0
+
+    private var rect = Rect()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
 
-    init {
-
+    private val updateListener = ValueAnimator.AnimatorUpdateListener {
+        progress = (it.animatedValue as Float).toDouble()
+        invalidate()    // redraw the screen
+        requestLayout() // when rectangular progress dimension changes
     }
 
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+    init {
+        isClickable = true
+        valueAnimator = AnimatorInflater.loadAnimator(
+            this.context,
+            R.animator.loading_animator
+        ) as ValueAnimator
+        valueAnimator.addUpdateListener(updateListener)
 
     }
 
@@ -43,4 +57,75 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val paint = Paint()
+        rect = canvas?.getClipBounds()!!
+        paint.setColor(resources.getColor(R.color.colorPrimary))
+        canvas.drawRect(rect, paint).apply {
+            textAlignment = TEXT_ALIGNMENT_CENTER
+        }
+        invalidate()
+
+        val paintText = Paint().apply {
+            color = resources.getColor(R.color.white)
+            textAlignment = TEXT_ALIGNMENT_CENTER
+            textSize = 70f
+        }
+
+        val paintClicked = Paint()
+        if (buttonState == ButtonState.Clicked){
+            paintClicked.setColor(resources.getColor(R.color.colorAccent))
+            canvas.drawRect(0f, 0f, (widthSize*progress/100).toFloat(), heightSize.toFloat(), paintClicked)
+            invalidate()
+        }
+        if (buttonState == ButtonState.Completed){
+            valueAnimator.cancel()
+            isClickable = true
+            invalidate()
+        }
+
+
+        canvas.drawText(
+            "Download",
+            (widthSize/4).toFloat(),
+            (heightSize/2).toFloat(),
+            paintText
+        )
+        invalidate()
+    }
+
+    //action to do when the button is clicked
+    override fun performClick(): Boolean {
+        super.performClick()
+        //Once the button is clicked, we make non-clickable until the state become completed
+        //isClickable = false
+        valueAnimator.start()
+        buttonState = ButtonState.Clicked
+        return  true
+    }
+
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        widthSize = (widthSize*progress/100).toInt()
+    }
+
+    fun downloadFinished(){
+        //Once download is completed, the button should be clickable again
+        buttonState = ButtonState.Completed
+        invalidate()
+    }
+
+    /*
+    *When someone click onthe button without seleccting an item
+    * we show a message to him inviting him to select something
+    *
+     */
+    fun selectItem(){
+        //we show a message then putting the button clickable again
+        Toast.makeText(context, "Please select an item", Toast.LENGTH_SHORT).show()
+        buttonState = ButtonState.Completed
+        invalidate()
+    }
 }
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.annotation.SuppressLint
 import android.app.DownloadManager
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,6 +10,8 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,19 +34,38 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            if(radio_group.checkedRadioButtonId != -1) {
+                download()
+            }
+            else{
+                custom_button.selectItem()
+            }
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
+        @SuppressLint("Range")
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if(id == downloadID){
+                custom_button.downloadFinished()
+            }
         }
     }
 
+    @SuppressLint("Range")
     private fun download() {
+
+        //parse the url with the selected option
+        val checkedButton = radio_group.checkedRadioButtonId
+        var url = when(checkedButton){
+            radio_glide.id -> URL_GLIDE
+            radio_retrofit.id -> URL_RETROFIT
+            else -> URL
+        }
+
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -53,11 +75,62 @@ class MainActivity : AppCompatActivity() {
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        var donwloadFinished = false
+        while (!donwloadFinished) {
+            val cursor =
+                downloadManager.query(DownloadManager.Query().setFilterById(downloadID));
+            while (cursor.moveToNext()) {
+                val status =
+                    cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                when (status) {
+                    DownloadManager.STATUS_FAILED -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "Download failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Log.i("applicationContext", "download failed")
+                        donwloadFinished = true
+                    }
+                    DownloadManager.STATUS_SUCCESSFUL -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "Download succeed",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        Log.i("applicationContext", "download succeed")
+                        donwloadFinished = true
+                    }
+                    DownloadManager.STATUS_PENDING -> {
+                        Log.i(
+                            "applicationContext",
+                            "Download pending"
+                        )
+                    }
+                    DownloadManager.STATUS_RUNNING -> {
+                        Log.i(
+                            "applicationContext",
+                            "Download runing"
+                        )
+                    }
+                    DownloadManager.STATUS_PAUSED -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "Download paused",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        Log.i("applicationContext", "download paused")
+                    }
+                }
+            }
+        }
     }
 
     companion object {
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL_RETROFIT = "https://github.com/square/retrofit"
+        private const val URL_GLIDE = "https://github.com/bumptech/glide"
         private const val CHANNEL_ID = "channelId"
     }
 
